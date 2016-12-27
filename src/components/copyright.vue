4a42cf4a00b02ce33607c87a88d4efa95de61196
程泽赫 <varrantczh@sina.com>
2016-12-27 14:41:54 +0800

    new

---
+++ src/components/copyright.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="index-bottom-info text-center text-overflow">
+        <p><i class="fa fa-copyright"></i><span> 2015-2016 All Rights Reserved.</span><span class="hidden-xs">Designed by Songtao.渝ICP备16000583号.</span></p>
+    </div>
+</template>
+<style scoped lang="scss">
+    .index-bottom-info {
+        user-select: none;
+        -webkit-user-select: none;
+        position: relative;
+        width: auto;
+        color: #eee;
+        font-size: 12px;
+        p {
+            overflow: hidden;
+        }
+    }
+</style>
